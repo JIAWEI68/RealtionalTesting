3eaa0e0b2af5310067889a380ca275621b4d765b
JIAWEI68 <ang.jiawei56@gmail.com>
2021-04-23 00:07:15 +0800

    Realtion Application Testing V1

---
+++ RealtioanlTesting/src/main/java/com/example/realtioanltesting/testRelational.java
@@ -0,0 +1,15 @@
+package com.example.realtioanltesting;
+
+public class testRelational {
+    public static void main(String[] args) {
+         int a = 10;
+         int b = 20;
+
+        System.out.println(" a == b  =" + (a==b));
+        System.out.println(" a != b = " + (a!=b));
+        System.out.println("a > b = " + (a>b));
+        System.out.println("a < b = " + (a<b));
+        System.out.println("a >= b = " + (a>=b));
+        System.out.println(" a <= b = " + (a<=b));
+    }
+}
